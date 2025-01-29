c46c2c5f819308a3c238ed5efbaae26e04357379
Firmanrachmad <dragobniel@gmail.com>
2025-01-29 16:29:26 +0700

    feat(api): fetch team data from loopback api

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,3 +1,10 @@
 import Route from '@ember/routing/route';
 
-export default class IndexRoute extends Route {}
+export default class IndexRoute extends Route {
+    async model() {
+        // Fetch data from the API
+        const response = await fetch('http://localhost:3000/api/teams');
+        const teams = await response.json();
+        return teams; // Return the fetched data
+    }
+}
