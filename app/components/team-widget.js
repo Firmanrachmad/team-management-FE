1874446365760a5fed72096ef15fba841389a6df
Firmanrachmad <dragobniel@gmail.com>
2025-01-29 16:07:49 +0700

    style(homepage): add parents-children templates, team widget components, and buttons

---
+++ app/components/team-widget.js
@@ -0,0 +1,20 @@
+import Component from '@glimmer/component';
+
+export default class TeamWidgetComponent extends Component {
+  // Array of available icons
+  icons = [
+    'fa-user',
+    'fa-users',
+    'fa-cogs',
+    'fa-lightbulb',
+    'fa-rocket',
+    'fa-star'
+  ];
+
+  // Function to get a random icon
+  get randomIcon() {
+    const randomIndex = Math.floor(Math.random() * this.icons.length);
+    return this.icons[randomIndex];
+  }
+
+}
