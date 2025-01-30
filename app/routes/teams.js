8397526b51a66f2c566558f03287a5b49e79e1c0
Firmanrachmad <dragobniel@gmail.com>
2025-01-30 10:44:10 +0700

    feat(api): fetch member data from loopback api

--- app/routes/teams.js
+++ app/routes/teams.js
@@ -2,10 +2,17 @@ import Route from '@ember/routing/route';
 
 export default class TeamsRoute extends Route {
   async model(params) {
-    let response = await fetch(
+    let teamResponse = await fetch(
       `http://localhost:3000/api/teams/${params.team_id}`
     );
-    let team = await response.json();
-    return team;
+    let team = await teamResponse.json();
+
+    let memberResponse = await fetch(
+      `http://localhost:3000/api/teams/${params.team_id}/members`
+    );
+    let member = await memberResponse.json();
+    console.log(member)
+
+    return { team, member };
   }
 }
